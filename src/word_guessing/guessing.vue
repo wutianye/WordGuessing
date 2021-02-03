b9c1e8ce0b3f26be8726d2f7d1e840ffbb423058
1175002048@qq.com <117>
2021-02-03 23:00:37 +0800

    添加猜词基础功能

--- src/word_guessing/guessing.vue
+++ src/word_guessing/guessing.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="page">
-    <div
-      class="list"
-      v-for="(item, index) in dataList"
-      :key="index"
-      @click="handleClick(item)"
-    >
-      {{ item }}
+    <div class="list"
+         v-for="(item, index) in dataList"
+         :key="index"
+         @click="handleClick(item, index)">
+      <div v-if="isGuessed[index] === false"
+           style="width:100% height:100%">
+        {{ item }}
+      </div>
+      <div v-else
+           style="width:100% height:100%">
+
+        <el-image :src="require('../assets/card-blue-back-2.png')"
+                  fit="fill"></el-image>
+
+      </div>
+
     </div>
   </div>
 </template>
@@ -16,21 +25,49 @@ import axios from "axios";
 export default {
   data() {
     return {
-      dataList: []
+      dataList: [],
+      isGuessed: []
     };
   },
   mounted() {
     this.getData();
   },
   methods: {
-    handleClick(val) {
-      alert(val);
+    handleClick(val, index) {
+      this.$confirm('此操作将标记这个词语, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.showIdentity(val, index)
+      }).catch(() => {
+        this.$message({
+          showClose: true,
+          type: 'info',
+          message: '已取消标记'
+        });
+      });
     },
+
+    //初始化猜词数据
     getData() {
       axios.get("/static/json/data.json").then(res => {
         console.log(res.data);
         this.dataList = res.data.data;
       });
+      for (var i = 0; i < 25; i++) {
+        this.isGuessed[i] = false;
+      }
+
+    },
+
+    showIdentity(val, index) {
+      //这里调用后台，来获得身份信息
+      console.log(this.isGuessed[index]);
+      this.$set(this.isGuessed, index, true)
+      console.log(this.isGuessed[index]);
+      //根据返回值加载图片
+
     }
   }
 };
