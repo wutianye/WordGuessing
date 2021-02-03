76a01259bf4d8e38ace4cdd0d872d8907d372d8a
renzhenfei <1367230472@qq.com>
2021-02-03 10:51:54 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-
     <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 html,
 body {
-  height: 100%;
   width: 100%;
+  height: 100%;
+  padding: 0;
   margin: 0;
 }
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  padding: 50px;
-  height: 100%;
+  box-sizing: border-box;
   width: 100%;
-  overflow: hidden;
+  height: 100%;
+  padding: 50px;
 }
 </style>
--- src/word_guessing/guessing.vue
+++ src/word_guessing/guessing.vue
@@ -1,86 +1,58 @@
 <template>
   <div class="page">
-    <div class="list"> {{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-
-    <div class="list">{{dataList[0]}}</div>
-
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
-    <div class="list">{{dataList[0]}}</div>
+    <div
+      class="list"
+      v-for="(item, index) in dataList"
+      :key="index"
+      @click="handleClick(item)"
+    >
+      {{ item }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      dataList: ['恒星', '象牙',
-        '老板娘',
-        '血',
-        '指挥家',
-        '汽油',
-        '假期',
-        '夹克',
-        '技师',
-        '天才',
-        '鞋',
-        '射线',
-        '绳子',
-        '盲点',
-        '罗宾汉',
-        '飞鸟',
-        '显微镜',
-        '坑',
-        '垃圾桶',
-        '浆果',
-        '寺庙',
-        '痕迹',
-        '鹰',
-        '喷射',
-        '自行车',
-        '企鹅',
-        '书签',
-        '遗体',
-        '恶作剧',
-        '易拉罐'
-      ]
+      dataList: []
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    handleClick(val) {
+      alert(val);
+    },
+    getData() {
+      axios.get("/static/json/data.json").then(res => {
+        console.log(res.data);
+        this.dataList = res.data.data;
+      });
     }
   }
-}
+};
 </script>
 
 <style scoped>
 .page {
   display: flex;
-  justify-content: space-between;
-  height: 100%;
-  width: 100%;
   flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
 }
 .list {
-  height: 18%;
-  width: 18%;
-  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: calc(100% / 5 - 20px);
+  height: calc(100% / 5 - 20px);
+  cursor: pointer;
   background-color: #cad5eb;
+  border-radius: 10px;
 }
 </style>
